15d8a5235984f4e4aea02e10c31f5f41ce6e0850
Esron Toledo <esron.toledo@localiza.com>
2021-03-03 13:39:15 -0300

    Novo teste

--- pages/api/cache/[ambiente].ts
+++ pages/api/cache/[ambiente].ts
@@ -3,12 +3,26 @@ import { NextApiRequest, NextApiResponse } from "next";
 const ambientes = ['hom', 'prd', 'rolbh', 'rolsp']
 
 export default async (resquest: NextApiRequest, response: NextApiResponse) => {
+    let environmentBody = resquest.query.ambiente as string;
 
-    let ambiente = resquest.query.ambiente as string;
+    let environment = getEnvironment().find(a => a.environment === environmentBody);
+
     response.status(200).json({
         mensagem: 'O cache foi limpo com sucesso.',
-        ambiente: ambiente
+        environmentBody: environmentBody,
+        environment: environment.environment,
+        environmentUrl: environment.url
     });
+}
+
+const clearCache = async () => {
+
+}
+
+const getEnvironment = () => {
+    return [
+        { environment: 'hom', url: 'teste' },
+    ];
 }
 
 
