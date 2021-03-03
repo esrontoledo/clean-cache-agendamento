fa2aa28547d876d0ddd81088d77c69f39235ac2e
Esron Toledo <esron.toledo@localiza.com>
2021-03-03 13:27:31 -0300

    Teste no erro 500

--- pages/api/cache/[ambiente].ts
+++ pages/api/cache/[ambiente].ts
@@ -5,47 +5,12 @@ const ambientes = ['hom', 'prd', 'rolbh', 'rolsp']
 export default async (resquest: NextApiRequest, response: NextApiResponse) => {
 
     let ambiente = resquest.query.ambiente as string;
-    const ambientesEscolhido = populateAmbientes().find(a => a.name === ambiente);
-    try {
-        if (!ambientesEscolhido) {
-            response.status(401).json({
-                error: `Ambiente não foi encontrado, por favor selecione um desses ambientes: (${ambientes.toString()})`
-            })
-        }
-        await cleanCahe(ambientesEscolhido);
-    }
-    catch (e) {
-        response.status(500).json({
-            error: 'Erro ao limpar o cache',
-            excecao: e
-        });
-    }
-
     response.status(200).json({
         mensagem: 'O cache foi limpo com sucesso.',
-        ambiente: ambientesEscolhido.name,
-        url: ambientesEscolhido.url
+        ambiente: ambiente
     });
 }
 
 
-const cleanCahe = async (ambienteEscolhido: { name: string, url: string }) => {
 
-    for (let item = 0; item <= 80; item++) {
-        const response = await fetch(ambienteEscolhido.url, {
-            method: 'Get',
-            credentials: 'include'
-        });
 
-        const json = await response.json();
-    }
-}
-
-const populateAmbientes = () => {
-    return [
-        { name: 'hom', url: 'http://ws-h.localiza.com/Operacoes/OP.Agendamento.API/api/util/limparCache?senha=AGENDAMENTO2019' },
-        { name: 'prd', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API/api/util/limparCache?senha=AGENDAMENTO2019' },
-        { name: 'rolbh', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API_Rollout_BH/api/util/limparCache?senha=AGENDAMENTO2019' },
-        { name: 'rolsp', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API_Rollout_SP/api/util/limparCache?senha=AGENDAMENTO2019' }
-    ];
-}
