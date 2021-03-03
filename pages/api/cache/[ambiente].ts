9c70e153aa8379f6771e2574c4961af43f11518e
Esron Toledo <esron.toledo@localiza.com>
2021-03-03 13:53:23 -0300

    novo comite

--- pages/api/cache/[ambiente].ts
+++ pages/api/cache/[ambiente].ts
@@ -3,13 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (resquest: NextApiRequest, response: NextApiResponse) => {
     let environmentBody = resquest.query.ambiente as string;
 
-    let environment = getEnvironment().find(a => a.environment === environmentBody);
+    let environment = getEnvironment().find(a => a.environment === environmentBody); //erro 
 
-    if (!environment) {
-        response.status(500).json({
-            error: 'Erro',
-        });
-    }
+    await clearCache(environment);
 
     response.status(200).json({
         mensagem: 'O cache foi limpo com sucesso.',
@@ -19,16 +15,24 @@ export default async (resquest: NextApiRequest, response: NextApiResponse) => {
     });
 }
 
-const clearCache = async () => {
+const clearCache = async (environment: { environment: string, url: string }) => {
+    for (let item = 0; item <= 80; item++) {
+        const response = await fetch(environment.url, {
+            method: 'Get',
+            credentials: 'include'
+        });
+        const json = await response.json();
 
+        console.log(json, environment.url);
+    }
 }
 
 const getEnvironment = () => {
     return [
-        { environment: 'hom', url: 'teste' },
-        { environment: 'prd', url: 'teste1' },
-        { environment: 'rolbh', url: 'teste2' },
-        { environment: 'rolsp', url: 'teste3' }
+        { environment: 'hom', url: 'http://ws-h.localiza.com/Operacoes/OP.Agendamento.API/api/util/limparCache?senha=AGENDAMENTO2019' },
+        { environment: 'prd', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API/api/util/limparCache?senha=AGENDAMENTO2019' },
+        { environment: 'rolbh', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API_Rollout_BH/api/util/limparCache?senha=AGENDAMENTO2019' },
+        { environment: 'rolsp', url: 'http://ws.localiza.com/Operacoes/OP.Agendamento.API-Rollout_SP/api/util/limparCache?senha=AGENDAMENTO2019' }
     ];
 }
 
